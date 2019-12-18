3b5562a35df9e1e67a47d3f388d1c55d94e11070
ShahzaibWaseem <shahzaib.waseem96@gmail.com>
2019-12-19 04:35:05 +0500

    Adding Command Line Argument Support for both the files (Child.py & Parent.py

--- Code/Child.py
+++ Code/Child.py
@@ -1,16 +1,35 @@
-import matplotlib.pyplot as plt
-from socket import *
 import time
+import argparse
+from socket import *
+import matplotlib.pyplot as plt
+
+def argumentHandling():
+	# Description
+	text="This is a Communication Code between two devices working on UDP.\nIn this the Child sends the data to the Parent and the parent tells when its buffer is to its limit then the data rate is slowed.\nThe Normal Parameters, if both code Files are run on the same machine, are as follows:\n\npython3 Child.py --address 127.0.0.1 --port 30000 --iterations 100 --mulFactor 2 --sleep 5"
+
+	# Command Line Arguments Parser
+	parser = argparse.ArgumentParser(description = text, formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-A", "--address", help="give the IP Address of the Parent", type=str, required=True)
+	parser.add_argument("-P", "--port", help="give the Port through which the data will go through", type=int, required=True)
+	parser.add_argument("-I", "--iterations", help="give the number of iterations", type=int, required=True)
+	parser.add_argument("-M", "--mulFactor", help="specify the Multiplication Factor", type=int, required=True)
+	parser.add_argument("-S", "--sleep", help="specify the Sleep time for each iteration", type=int, required=True)
+
+	args = parser.parse_args()
+
+	# Printing
+	print("The IP Address to connect to is\t\t\t:\t", args.address)
+	print("The Port Number to connect to is\t\t:\t", args.port)
+	print("The Number of iterations are\t\t\t:\t", args.iterations)
+	print("The Multiplication/Division Factor is\t\t:\t", args.mulFactor)
+	print("The Normal Sleep time for each iteration is\t:\t", args.sleep)
+
+	return args.address, args.port, args.iterations, args.mulFactor, args.sleep
 
 # Global Variables
-PARENT_IP = "127.0.0.1"
-PARENT_PORT = 30000
-
-ITERATIONS=100
-MULTIPLICATION_FACTOR=2
 RECIEVE_SIZE=2048
 
-sleep_time=5
+PARENT_IP, PARENT_PORT, ITERATIONS, MULTIPLICATION_FACTOR, sleep_time=argumentHandling()
 speed=[]
 
 def main():
@@ -18,7 +37,7 @@ def main():
 	# UDP
 	Socket = socket(AF_INET, SOCK_DGRAM)
 	# Sending number of iterations
-	print("Pinging...\nSending number of iterations (", str(ITERATIONS),") to the parent")
+	print("\nPinging...\nSending number of iterations (", str(ITERATIONS),") to the parent")
 	Socket.sendto(str(ITERATIONS).encode("utf-8"), (PARENT_IP, PARENT_PORT))
 
 	print("\nIteration Number\t|\tVariation in Speed\t|\tWaiting Time")
--- Code/Parent.py
+++ Code/Parent.py
@@ -1,18 +1,32 @@
 import sys
 import time
+import argparse
 from socket import *
 from threading import Lock, Thread
 
-# Global Variables
-IP = "127.0.0.1"
-PORT = 30000
+def argumentHandling():
+	# Description
+	text="This is a Communication Code between two devices working on UDP.\nIn this the Child sends the data to the Parent and the parent tells when its buffer is to its limit then the data rate is slowed.\nThe Normal Parameters, if both code Files are run on the same machine, are as follows:\n\npython3 Parent.py --address 127.0.0.1 --port 30000 --bufferSize 10"
+	# Command Line Arguments Parser
+	parser = argparse.ArgumentParser(description = text, formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-A", "--address", help="give the IP Address of the Parent", type=str, required=True)
+	parser.add_argument("-P", "--port", help="give the Port through which the data will come through", type=int, required=True)
+	parser.add_argument("-B", "--bufferSize", help="give the Size of the buffer", type=int, required=True)
 
-BUFFER_SIZE=8
+	args = parser.parse_args()
+
+	print("The IP Address to connect to is\t\t:\t", args.address)
+	print("The Port Number to connect to is\t:\t", args.port)
+	print("The Size of the Buffer is\t\t:\t", args.bufferSize)
+
+	return args.address, args.port, args.bufferSize
+
+# Global Variables
+IP, PORT, BUFFER_SIZE = argumentHandling()
 RECIEVE_SIZE=2048
 
 thread = None
 lock = Lock()
-
 Buffer=[]
 buffer_filled=0
 
@@ -35,15 +49,15 @@ def main():
 	Socket = socket(AF_INET, SOCK_DGRAM)
 	Socket.bind((IP, PORT))
 
-	print("Pinging...\nAsking for the number of iterations from the child")
+	print("\nPinging...\nAsking for the number of iterations from the child")
 	iterations, childAddress=Socket.recvfrom(RECIEVE_SIZE)
 
 	thread=Thread(target=outFlow)
-	thread.daemon=True
+	thread.daemon=True	# Setting thread as daemon helps with exiting from the thread
 	thread.start()
 
 	print("\nRunning Code for", iterations.decode(), "Iterations")
-	print("\nIteration Number\t|\tBuffer Contents", end="")
+	print("\nIteration Number\t|\tBuffer Contents\t\t\t\t|\tDropping or Not", end="")
 
 	for iteration in range(int(iterations.decode())):
 		message, childAddress = Socket.recvfrom(RECIEVE_SIZE)
@@ -56,14 +70,14 @@ def main():
 			lock.release()
 			print()
 		else:
-			print("\t\tDropping next item (".rjust(25), message.decode(), ")")
+			print("\t|\tDropping next item (".rjust(25), message.decode(), ")")
 			Socket.sendto("OverFlow\nDecrease the speed".encode(), childAddress)
 
 		print(iteration, "\t\t\t|\t", *Buffer, end="\t")
 
 	print()
 	Socket.close()
-	sys.exit()
+	sys.exit()			# Responsible for Exiting the Code
 
 if __name__ == '__main__':
 	main()
